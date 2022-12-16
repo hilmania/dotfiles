a5b9614a3db92904329e3c15cac94b8f6fdf3a38
Hilman Ibnu Assiddiq <hia.develop@gmail.com>
2022-12-16 08:17:35 +0700

    update(wezterm): add togglefullscreen, remove window and tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,17 @@ local wezterm = require 'wezterm'
 
 return {
     
-    font = wezterm.font 'Fira Code',
-    color_scheme = "Darcula (base16)",
-    default_prog = { '/usr/local/bin/fish', '-l' },
-    font_size = 16.0,
-    line_height = 1.4,
+   font = wezterm.font 'Fira Code',
+    color_scheme = "tokyonight",
+    font_size = 14.0,
+    line_height = 1.3,
+    window_decorations = "NONE",
+    enable_tab_bar = false,
+    keys = {
+        {
+            key = 'n',
+            mods = 'SHIFT|CTRL',
+            action = wezterm.action.ToggleFullScreen,
+        },
+    },
 }
