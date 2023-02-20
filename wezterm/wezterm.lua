a3c1a4e36caacda99f42795580c2faafbf075fe7
Hilman Ibnu Assiddiq <hia.develop@gmail.com>
2023-02-20 13:16:50 +0700

    update(major): config for portable development

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,17 +2,11 @@ local wezterm = require 'wezterm'
 
 return {
     
-   font = wezterm.font 'Fira Code',
+   font = wezterm.font 'FiraCode Nerd Font',
     color_scheme = "tokyonight",
     font_size = 14.0,
     line_height = 1.3,
     window_decorations = "NONE",
     enable_tab_bar = false,
-    keys = {
-        {
-            key = 'n',
-            mods = 'SHIFT|CTRL',
-            action = wezterm.action.ToggleFullScreen,
-        },
-    },
+    
 }
