a9be95813a8b8842937a6af1cb6d110b54cc49a9
Hilman Ibnu Assiddiq <hia.develop@gmail.com>
2023-03-01 15:56:56 +0700

    update(nvim): add plugin rest nvim, update keymaps

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -81,3 +81,6 @@ keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
 
 -- Lsp
 keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+
+-- REST Nvim 
+keymap("n", "<S-t>", "<cmd>lua require('rest-nvim').run()<cr>", opts)
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -101,6 +101,46 @@ return packer.startup(function(use)
   use { "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" }
   use { "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" }
 
+  --REST Nvim 
+  use { "rest-nvim/rest.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("rest-nvim").setup({
+          -- Open request results in a horizontal split
+          result_split_horizontal = false,
+          -- Keep the http file buffer above|left when split horizontal|vertical
+          result_split_in_place = false,
+          -- Skip SSL verification, useful for unknown certificates
+          skip_ssl_verification = false,
+          -- Encode URL before making request
+          encode_url = true,
+          -- Highlight request on run
+          highlight = {
+            enabled = true,
+            timeout = 150,
+          },
+          result = {
+            -- toggle showing URL, HTTP info, headers at top the of result window
+            show_url = true,
+            show_http_info = true,
+            show_headers = true,
+            -- executables or functions for formatting response body [optional]
+            -- set them to false if you want to disable them
+            formatters = {
+              json = "jq",
+              html = function(body)
+                return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
+              end
+            },
+          },
+          -- Jump to request line on run
+          jump_to_request = false,
+          env_file = '.env',
+          custom_dynamic_variables = {},
+          yank_dry_run = true,
+        })
+    end
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
