c33ea81d5e102ad94a48bdbc9cdb8d170df1ff85
Hilman Ibnu <hia.develop@gmail.com>
2025-02-13 16:03:16 +0700

    fix: highlight blade treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,27 @@ return {
     ensure_installed = {
       "lua",
       "vim",
-      -- add more arguments for adding more treesitter parsers
+      "blade",
     },
   },
+  config = function(_, opts)
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
+
+    vim.filetype.add {
+      pattern = {
+        [".*%.blade%.php"] = "blade",
+      },
+    }
+
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
